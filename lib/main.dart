3afe124d97cf49d0e89a34461bda58eb7ca03120
Adner Scarpelini <49483327+adnerscarpelini@users.noreply.github.com>
2020-06-17 13:04:50 -0300

    Função para Remover
    Função para Remover tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,10 @@ class _HomeState extends State<Home> {
 
   List _toDoList = [];
 
+  //ADNER: Para saberr o item e a posição que foi removido na exclusão
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
   //ADNER: Sobrescreve o método que sempre é executado ao abrir o aplicativo
   //E nele realiza a consulta do JSON com os dados salvos no arquivo JSON
   @override
@@ -89,27 +93,78 @@ class _HomeState extends State<Home> {
                 padding: EdgeInsets.only(top: 10.0),
                 //Passo quantos itens vai ter a lista
                 itemCount: _toDoList.length,
-                itemBuilder: (context, index) {
-                  return CheckboxListTile(
-                    title: Text(_toDoList[index]["title"]),
-                    value: _toDoList[index]["ok"],
-                    secondary: CircleAvatar(
-                      child: Icon(
-                          //Se tiver "OK" coloca o icone marcado
-                          _toDoList[index]["ok"] ? Icons.check : Icons.error),
-                    ),
-                    onChanged: (c) {
-                      //ADNER: Evento ao marcar ou desmarcar o checkbox
-                      setState(() {
-                        _toDoList[index]["ok"] = c;
-                        _saveData();
-                      });
-                    },
-                  );
-                }),
+                itemBuilder: _buildItem),
           )
         ],
       ),
+    );
+  }
+
+  //ADNER: Função para construir os itens da lista
+  Widget _buildItem(BuildContext context, int index) {
+    //O Dismissible é o recurso que permite deslizar para a direita abrindo
+    //o menu de exclusão
+    return Dismissible(
+      //A key é nessaria para dar um nome, porém ele exige que seja criado
+      //um por execução, então pego o tempo atual em milisegundos
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          //Alinha o ícone 90% pra esquerda
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      //Direita para esquerda
+      //O filho vai ser a lista (Icone de check e Texto da Tarefa)
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(
+              //Se tiver "OK" coloca o icone marcado
+              _toDoList[index]["ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (c) {
+          //ADNER: Evento ao marcar ou desmarcar o checkbox
+          setState(() {
+            _toDoList[index]["ok"] = c;
+            _saveData();
+          });
+        },
+      ),
+      //Ao deslizar efetue a exclusão do item
+      onDismissed: (direction) {
+        setState(() {
+          //Pegar o item que está sendo removido e salvar para poder desfazer
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.remove(index);
+
+          _saveData();
+
+          //Mostrar o snackbar com sucesso e opção de desfazer
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
+            ),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
